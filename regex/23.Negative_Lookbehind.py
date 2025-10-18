845e18c57c7eec8742469a56b94f5f10c03315cd
nawrin15 <sadianawrin93@gmail.com>
2025-10-18 13:26:05 +0600

    update

--- regex/23.Negative_Lookbehind.py
+++ regex/23.Negative_Lookbehind.py
@@ -1,41 +1,46 @@
-# (?<=regex_2)regex_1
-# The positive lookbehind (?<=) asserts regex_1 to be immediately preceded by regex_2. 
+# (?<regex_2)regex_1
+#The negative lookbehind (?<!) asserts regex_1 not to be immediately preceded by regex_2. 
 # Lookbehind is excluded from the match (do not consume matches of regex_2), 
 # but only assert whether a match is possible or not.
 
 # Task
 
 # You have a test String S.
-# Write a regex which can match all the occurences of digit which are immediately preceded by odd digit.
+# Write a regex which can match all the occurences of characters 
+# which are not immediately preceded by vowels (a, e, i, u, o, A, E, I, O, U).
+
+
+Regex_Pattern = r"(?<![a-z])[aeiou]"
+
 import re
 
-Regex_Pattern = r"(?<=[13579])\d"
-Test_String = "123Go!"
+Test_String = "he1o"
 
 match = re.findall(Regex_Pattern, Test_String)
 
 print("Number of matches :", len(match), match)
 
-Test_String = "123456789"
+Regex_Pattern = r"(?<![aeiouAEIOU])(.)"
+
+Test_String = "1o1s"
 
 match = re.findall(Regex_Pattern, Test_String)
 
 print("Number of matches :", len(match), match)
 
-Test_String = "1349876562bsdbcjksdc bsjdc11231231sc nsdj cs,c n"
+Test_String = "thisisavowel"
 
 match = re.findall(Regex_Pattern, Test_String)
 
 print("Number of matches :", len(match), match)
 
-Test_String = "11111"
+Test_String = "1qaz2wsx3edc4rfv5tgb6yhn7ujm8ik9ool./;p01QAZ2WSX3EDC4RFV5TGB6YHN7UJM8IK,9OL.0P;/-['"
 
 match = re.findall(Regex_Pattern, Test_String)
 
 print("Number of matches :", len(match), match)
 
-Regex_Pattern = r"(?<=[a-z])[aeiou]"
-Test_String = "he1o!"
+Test_String = "SC HUIEHKESYG VSBC SF BDS GUIHuhjsd bcukhb2ip4hjjvs bvfdsbvl bv"
 
 match = re.findall(Regex_Pattern, Test_String)
 
